d64d5f6a0f10c14e2d7703a22811a4d91bd8d8b2
shewalenikita2222-dev <shewalenikita2222@gmail.com>
2025-10-25 21:05:22 +0530

    Add files via upload

---
+++ LibrarySystem/User.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private int userId;
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int getUserId() { return userId; }
+    public String getName() { return name; }
+    public List<Book> getBorrowedBooks() { return borrowedBooks; }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public String toString() {
+        return userId + " | " + name + " | Borrowed: " + borrowedBooks.size();
+    }
+}
